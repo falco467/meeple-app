ee549b47f69ccfe9b13820d64e66a93e26e74497
Falco <falco467@gmail.com>
2024-08-07 12:14:47 +0200

    added note to prevent accidental commit

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,5 +1,6 @@
 export const firebaseConfig = {
   // Paste your configuration here from Firebase console -> Project Settings -> general -> web app
+  // To prevent accidental commit/push of this file run: git update-index --skip-worktree public/firebaseConfig.js
 }
 
 // Paste your messaging vapid key here from Firebase console -> Project Settings -> Cloud Messaging -> Key Pair
